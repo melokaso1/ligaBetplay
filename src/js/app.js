2ddce79135e0507e3893b2a552d7952bca1d48e3
Alejandro escobar <107068494+melokaso1@users.noreply.github.com>
2025-10-25 10:26:14 -0500

    feat: :sparkles: diseño, y botones creados

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,17 @@
 import "../css/style.css";
+
+let container = document.getElementById("body-info");
+
+function tabla() {
+  let html = `
+  `;
+  container.innerHTML = html;
+}
+
+function gestion(){
+  let html = `
+  `
+  container.innerHTML = html
+}
+
+tabla();
